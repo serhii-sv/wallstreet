63071e6b8e98af6c94c49951488aebe89006c5f4
Serhii V <serhii.voronovych@gmail.com>
2021-06-29 15:01:23 +0800

    - update

--- resources/views/customer/contact.blade.php
+++ resources/views/customer/contact.blade.php
@@ -11,11 +11,11 @@
                 <h2 class="page-title page-title--line">{{ __('We are always glad to see you') }} <span>{{ __('Write to us') }}</span>
                 </h2>
             </div>
-            {{--<section class="map">--}}
-                {{--<div class="container">--}}
-                    {{--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11277.525848972855!2d38.98303972015857!3d45.037480404734495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f04f9c2224cafb%3A0x5c1c5247a63a9634!2z0JPQsNC70LXRgNC10Y8g0JrRgNCw0YHQvdC-0LTQsNGA!5e0!3m2!1sru!2sru!4v1538048912624" frameborder="0" style="border:0" allowfullscreen=""></iframe>--}}
-                {{--</div>--}}
-            {{--</section>--}}
+            <section class="map">
+                <div class="container">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3057.267277897746!2d116.36390971546201!3d39.980131990000096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35f0547c077c114d%3A0xc5a587b872450a4a!2sSyncept%20Business%20Center!5e0!3m2!1sid!2sid!4v1624950034869!5m2!1sid!2sid" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </section>
             <div class="text-and-form">
                 <div class="container">
                     <div class="text-and-form__content">
