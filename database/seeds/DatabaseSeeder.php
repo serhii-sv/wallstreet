ed27bdfa0706835f278eb8203d6f9c787b8a28a3
Serhii V <serhii.voronovych@gmail.com>
2021-07-16 14:12:43 +0800

    - update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,7 +21,6 @@ class DatabaseSeeder extends Seeder
             TransactionTypesSeeder::class,
             CountriesSeeder::class,
             SettingSeeder::class,
-            TranslationsSeeder::class,
             TelegramBotSeeder::class,
             TaskScopesSeeder::class,
         ]);
