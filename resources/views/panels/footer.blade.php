d3423ec599c77de0e69e82990015cc6bbb31f8b1
Serhii V <serhii.voronovych@gmail.com>
2021-07-30 19:16:32 +0800

    - global cleaning.

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -3,11 +3,11 @@
   class="{{$configData['mainFooterClass']}} @if($configData['isFooterFixed']=== true){{'footer-fixed'}}@else {{'footer-static'}} @endif @if($configData['isFooterDark']=== true) {{'footer-dark'}} @elseif($configData['isFooterDark']=== false) {{'footer-light'}} @else {{$configData['mainFooterColor']}} @endif">
   <div class="footer-copyright">
     <div class="container">
-      <span>&copy; 2019 <a href="http://themeforest.net/user/pixinvent/portfolio?ref=pixinvent"
-          target="_blank">PIXINVENT</a> All rights reserved.
+      <span>&copy; {{ date('Y') }} <a href="https://factorcrm.co"
+          target="_blank">FactorCRM</a> All rights reserved.
       </span>
       <span class="right hide-on-small-only">
-        Design and Developed by <a href="https://pixinvent.com/">PIXINVENT</a>
+        With Developed by <a href="https://factorcrm.co/">FactorCRM</a>
       </span>
     </div>
   </div>
