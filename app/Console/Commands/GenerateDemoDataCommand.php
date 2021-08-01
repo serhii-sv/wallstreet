47f3e5a7209cab4820fcc62af359b1139f34897e
Serhii V <serhii.voronovych@gmail.com>
2021-08-01 11:45:37 +0800

    - global cleaning.

--- app/Console/Commands/GenerateDemoDataCommand.php
+++ app/Console/Commands/GenerateDemoDataCommand.php
@@ -64,7 +64,8 @@ class GenerateDemoDataCommand extends Command
     {
         parent::__construct();
 
-        $this->faker = Factory::create();
+        $this->stressLevel  = 1;
+        $this->faker        = Factory::create();
     }
 
     /**
@@ -104,9 +105,9 @@ class GenerateDemoDataCommand extends Command
             $this->generateSettings();
             $this->comment('Settings generated');
 
-//            $this->comment('Generating transactions');
-//            $this->generateTransactions();
-//            $this->comment('Transactions generated');
+            $this->comment('Generating transactions');
+            $this->generateTransactions();
+            $this->comment('Transactions generated');
         }
 
         $this->comment('Generating users');
@@ -342,8 +343,124 @@ class GenerateDemoDataCommand extends Command
             }
 
             $user = User::create($newUser);
-            GenerateDemoForUserJob::dispatch($user, $this->stressLevel)->delay(0);
+            $this->generateBalances($user);
+            $this->generateWithdrawals($user);
+            $this->generateDeposits($user);
             $this->info('user ' . $newUser['name'] . ' registered');
         }
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    private function generateBalances($user)
+    {
+        $transactionType = TransactionType::getByName('enter');
+
+        /** @var Wallet $wallet */
+        foreach ($user->wallets()->get() as $wallet) {
+            for($i = 1; $i <= $this->faker->numberBetween(3,5); $i++) {
+                $amount             = $this->faker->numberBetween(10, 1000);
+                $externalWallet     = 'W' . $this->faker->randomNumber(5);
+                $transactionData    = [
+                    'amount'            => $amount,
+                    'type_id'           => $transactionType->id,
+                    'user_id'           => $wallet->user_id,
+                    'wallet_id'         => $wallet->id,
+                    'currency_id'       => $wallet->currency_id,
+                    'payment_system_id' => $wallet->payment_system_id,
+                    'result'            => 'completed',
+                    'batch_id'          => 'B'.$this->faker->randomNumber(5),
+                    'approved'          => 1,
+                    'log'               => $this->faker->text,
+                    'created_at'        => $this->faker->dateTimeThisYear()->format('Y-m-d').' 12:00:00',
+                ];
+                $wallet->refill($transactionData['amount'], $externalWallet);
+                Transaction::create($transactionData);
+            }
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function generateWithdrawals($user)
+    {
+        $wallets = Wallet::where('user_id', $user->id)
+            ->where('balance', '>', 10)
+            ->inRandomOrder()
+            ->limit(1);
+
+        if (0 === $wallets->count()) {
+            return;
+        }
+
+        /** @var Wallet $wallet */
+        foreach ($wallets->get() as $wallet) {
+            /** @var Currency $currency */
+            $currency   = $wallet->currency()->first();
+            $amount     = $this->faker->randomFloat($currency->precision, 0.5, 5);
+
+            $transaction = Transaction::withdraw($wallet, $amount);
+
+            if (null !== $transaction && $this->faker->boolean) {
+                $transaction->approved = 1;
+                $transaction->save();
+            }
+        }
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    private function generateDeposits($user)
+    {
+        /** @var Rate $randomRate */
+        $randomRate = Rate::where('active', 1)
+            ->inRandomOrder()
+            ->limit(1)
+            ->first();
+
+        if (null === $randomRate) {
+            return;
+        }
+
+        $wallet = $user->wallets()
+            ->where('currency_id', $randomRate->currency_id)
+            ->first();
+
+        if (null === $wallet) {
+            return;
+        }
+
+        $enterTransaction    = TransactionType::getByName('enter');
+        $externalWallet      = 'W' . $this->faker->randomNumber(5);
+        $transactionData     = [
+            'amount'            => $randomRate->max,
+            'type_id'           => $enterTransaction->id,
+            'user_id'           => $wallet->user_id,
+            'wallet_id'         => $wallet->id,
+            'currency_id'       => $wallet->currency_id,
+            'payment_system_id' => $wallet->payment_system_id,
+            'result'            => 'completed',
+            'batch_id'          => 'B'.$this->faker->randomNumber(5),
+            'approved'          => 1,
+            'log'               => $this->faker->text,
+            'created_at'        => now(),
+        ];
+        $wallet->refill($transactionData['amount'], $externalWallet);
+        Transaction::create($transactionData);
+
+        $min = $randomRate->min == 0 ? 1 : $randomRate->min;
+        $depositAmount = $this->faker->numberBetween($min, $randomRate->max);
+        $depositData = [
+            'wallet_id'  => $wallet->id,
+            'rate_id'    => $randomRate->id,
+            'amount'     => $depositAmount,
+            'reinvest'   => $randomRate->reinvest ? $this->faker->numberBetween(0, 20) : 0,
+            'created_at' => now(),
+            'user'       => $wallet->user()->first(),
+        ];
+        Deposit::addDeposit($depositData);
     }
 }
