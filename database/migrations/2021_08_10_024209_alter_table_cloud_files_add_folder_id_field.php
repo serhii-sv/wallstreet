b1564771e101948055af2ad0aa2886d5ca9f904b
fladko <vlad.babenko@tewris.com>
2021-08-10 07:29:42 +0300

    cloud file folders

---
+++ database/migrations/2021_08_10_024209_alter_table_cloud_files_add_folder_id_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableCloudFilesAddFolderIdField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cloud_files', function (Blueprint $table) {
+            $table->foreignId('cloud_file_folder_id')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cloud_files', function (Blueprint $table) {
+            $table->dropForeign(['cloud_file_folder_id']);
+            $table->dropColumn('cloud_file_folder_id');
+        });
+    }
+}
