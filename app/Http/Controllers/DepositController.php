ed011211c837a0c8dd5c49a5a89d8e9461342523
fladko <vlad.babenko@tewris.com>
2021-08-22 15:54:54 +0300

    deposits close during week sattus filter

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -23,7 +23,7 @@ class DepositController extends Controller
 
     public function index(Request $request)
     {
-        $deposits_status = ['Не активные' => 'false', 'Активные' => 'true'];
+        $deposits_status = ['Не активные' => 'false', 'Активные' => 'true', 'Закрываются в течении недели' => 'close_during_week'];
         $deposits_count = Deposit::count();
 
         if (request()->ajax()) {
@@ -31,7 +31,12 @@ class DepositController extends Controller
             $filter_status = $request->get('status') ? $request->get('status') : false;
             $filter_rates = $request->get('rate') ? $request->get('rate') : false;
             $deposits = Deposit::when($filter_status, function($query) use ($filter_status){
-                return $query->where('active', $filter_status);
+                if ($filter_status == 'close_during_week') {
+                    return $query->where('active', true)
+                        ->where('datetime_closing', '<=', date('Y-m-d H:i:s', strtotime('+ 1 week')));
+                } else {
+                    return $query->where('active', $filter_status);
+                }
             })->when($filter_rates, function($query) use ($filter_rates){
                 return $query->where('rate_id', $filter_rates);
             })->orderBy('created_at', 'desc');
