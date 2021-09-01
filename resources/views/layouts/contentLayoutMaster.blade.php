3d8b79bacb0adbd178eb78b535ef63a0de6d271e
fladko <vlad.babenko@tewris.com>
2021-09-01 07:32:28 +0300

    add Tawk script to header

--- resources/views/layouts/contentLayoutMaster.blade.php
+++ resources/views/layouts/contentLayoutMaster.blade.php
@@ -36,6 +36,8 @@ License: You must have a valid license purchased only from themeforest(the above
   <link rel="apple-touch-icon" href="{{asset('images/favicon/apple-touch-icon-152x152.png')}}">
   <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon/favicon-32x32.png')}}">
 
+    <!--Start of Tawk.to Script--> <script type="text/javascript"> var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); (function(){ var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0]; s1.async=true; s1.src='https://embed.tawk.to/612dc757649e0a0a5cd3c665/1fedd5c13'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); })(); </script> <!--End of Tawk.to Script-->
+
   {{-- Include core + vendor Styles --}}
   @include('panels.styles')
 
