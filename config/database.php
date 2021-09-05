4dfdf16635884400827922de14c923a8397b9994
Николаев Дмитрий Николаевич <fnxrus@gmail.com>
2021-09-05 09:41:19 +0300

    cfg db

--- config/database.php
+++ config/database.php
@@ -58,6 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'unix_socket'   => env('UNIX_SOCKET',    '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
@@ -71,13 +72,14 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket'   => env('UNIX_SOCKET',    '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null
+            
         ],
 
         'pgsql' => [
