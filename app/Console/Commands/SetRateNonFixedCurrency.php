aef6201eb71f1088795fa9600f177d354a0327aa
Николаев Дмитрий Николаевич <fnxrus@gmail.com>
2021-09-30 00:12:46 +0300

    cur rates upd

--- app/Console/Commands/SetRateNonFixedCurrency.php
+++ app/Console/Commands/SetRateNonFixedCurrency.php
@@ -43,26 +43,34 @@ class SetRateNonFixedCurrency extends Command
             
             foreach ($exchange_rates as $exchange_rate) {
                 $currency_rate = Setting::find($exchange_rate->rate_id);
-                
+                $fixed_rate = $exchange_rate->rate;
                 if ($currency_rate !== null) {
                     if ($exchange_rate->is_random) {
-                        $rate = $exchange_rate->min_rate + mt_rand() / mt_getrandmax() * ($exchange_rate->max_rate - $exchange_rate->min_rate);
-                        $rate = round($rate, 8);
-                        $this->info('Текущий курс '. $currency_rate->s_key .' будет равен рандомному числу ' .$rate);
+                        if ($exchange_rate->min_rate !== null && $exchange_rate->max_rate !== null) {
+                            $rate = $exchange_rate->min_rate + mt_rand() / mt_getrandmax() * ($exchange_rate->max_rate - $exchange_rate->min_rate);
+                            $rate = round($rate, 8);
+                        }
                     } else {
                         $rate = $exchange_rate->rate;
-                        $this->info('Текущий курс '. $currency_rate->s_key .' будет равен ' . $rate);
                     }
-                    if ($exchange_rate->date_end > Carbon::now()) {
-                        if ($exchange_rate->date_start < Carbon::now()) {
+                    if ($exchange_rate->date_end !== null && $exchange_rate->date_end > Carbon::now()) {
+                        if ($exchange_rate->date_start !== null && $exchange_rate->date_start < Carbon::now()) {
                             $currency_rate->update(['s_value' => $rate]);
-                            $this->info('Текущий курс '. $currency_rate->s_key .' равен ' . $rate);
-                        }else{
-                            $this->info('Время еще не пришло для '. $currency_rate->s_key);
+                            $this->info('Текущий курс для' . $currency_rate->s_key . ' установлен ' . $rate);
+                        } else {
+                            $this->info('Время еще не пришло для ' . $currency_rate->s_key);
+                        }
+                    } else if ($exchange_rate->date_end === null) {
+                        if ($exchange_rate->date_start !== null && $exchange_rate->date_start < Carbon::now()) {
+                            $currency_rate->update(['s_value' => $rate]);
+                            $this->info('Текущий курс для' . $currency_rate->s_key . ' установлен ' . $rate);
+                        } else {
+                            $currency_rate->update(['s_value' => $fixed_rate]);
+                            $this->info('Текущий курс для' . $currency_rate->s_key . ' установлен ' . $fixed_rate);
                         }
                     } else {
                         $currency_rate->update(['s_value' => $exchange_rate->rate]);
-                        $this->info('Время прошло для '. $currency_rate->s_key. '. Курс на текущий момент будет равен ' . $exchange_rate->rate);
+                        $this->info('Время прошло для ' . $currency_rate->s_key . '. Курс на текущий момент будет равен ' . $exchange_rate->rate);
                     }
                 }
             }
