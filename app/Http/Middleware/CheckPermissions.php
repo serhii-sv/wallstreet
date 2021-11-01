fe22a73427d613c63a047db2dc8cdb1e46450395
Serhii V <serhii.voronovych@gmail.com>
2021-11-01 20:52:11 +0800

    update

--- app/Http/Middleware/CheckPermissions.php
+++ app/Http/Middleware/CheckPermissions.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\ModelHasPermission;
 use App\Models\Permission;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,15 +22,17 @@ class CheckPermissions
     public function handle(Request $request, Closure $next)
     {
         $authGuard = app('auth')->guard('web');
-    
+
         if ($authGuard->guest()) {
             throw UnauthorizedException::notLoggedIn();
         }
         $route_name = $request->route()->getName();
         $permission = Permission::where('slug', $route_name)->first();
-        
+
+        die(print_r(ModelHasPermission::where('model_id', auth()->user()->id)->get(),true));
+
         if (!empty($permission)){
-            
+
             if (Auth::check() && Auth::user()->can($permission->name)){
                 return $next($request);
             }else{
