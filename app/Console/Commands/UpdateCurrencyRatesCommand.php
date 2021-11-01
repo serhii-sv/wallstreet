fa1aff1dd5d3e525f934e248dbed67e9c7067bdb
Serhii V <serhii.voronovych@gmail.com>
2021-11-01 11:57:30 +0800

    - update

--- app/Console/Commands/UpdateCurrencyRatesCommand.php
+++ app/Console/Commands/UpdateCurrencyRatesCommand.php
@@ -68,8 +68,12 @@ class UpdateCurrencyRatesCommand extends Command
             }
 
             if (!isset($response['data'][strtoupper($currency->code)]['quote']['USD']['price'])) {
-                \Log::error('Can not get rate for ' . $currency->code . ' in USD');
-                continue;
+                if (strtoupper($currency->code) == 'USD') {
+                    $response['data'][strtoupper($currency->code)]['quote']['USD']['price'] = 1;
+                } else {
+                    \Log::error('Can not get rate for ' . $currency->code . ' in USD');
+                    continue;
+                }
             }
 
             $rateInUsd = (float)round($response['data'][strtoupper($currency->code)]['quote']['USD']['price'], $currency->precision);
