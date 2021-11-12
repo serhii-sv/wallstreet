ee6b53138c89229f528a478eaf9dfd826dab6646
Serhii V <serhii.voronovych@gmail.com>
2021-11-12 16:41:33 +0800

    update

--- app/Http/Controllers/ReplenishmentController.php
+++ app/Http/Controllers/ReplenishmentController.php
@@ -42,11 +42,11 @@ class ReplenishmentController extends Controller
             }
 
             if (!is_null($request->fake)) {
-                $transactions->where('is_real', 0);
+                $transactions->where('is_real', false);
             }
 
             if (!is_null($request->real)) {
-                $transactions->where('is_real', 1);
+                $transactions->where('is_real', true);
             }
 
             if (isset($request->search['value']) && !is_null($request->search['value'])) {
