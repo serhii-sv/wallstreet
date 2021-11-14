dee9a133f4b8f4955b217f16881a936ff89972d0
Serhii V <serhii.voronovych@gmail.com>
2021-11-14 16:00:08 +0800

    update

--- resources/views/pages/withdrawals/partials/upliner.blade.php
+++ resources/views/pages/withdrawals/partials/upliner.blade.php
@@ -1,4 +1,4 @@
-@php($partner = $transaction->user->firstPartner($transaction->partner))
+@php($partner = $transaction->user->firstPartner($transaction->user))
 @if(null !== $partner)
   <a href="{{ route('users.show', $partner->id) }}">{{ $partner->login }}</a>
 @endif
