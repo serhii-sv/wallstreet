7cea9b9407b4bba4a6b6b1895bb0f10bafc9271e
Serhii V <serhii.voronovych@gmail.com>
2021-11-14 16:33:33 +0800

    update

--- resources/views/pages/users/partials/partner.blade.php
+++ resources/views/pages/users/partials/partner.blade.php
@@ -1,4 +1,4 @@
-@php($partner = $transaction->user->firstPartner($transaction->user))
+@php($partner = $user->firstPartner($user))
 @if(null !== $partner)
     <a href="{{ route('users.show', $partner->id) }}">{{ $partner->login }}</a>
 @endif
