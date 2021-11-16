9cc065886cd95d4feffc5872970f7972d3bed764
Serhii V <serhii.voronovych@gmail.com>
2021-11-16 12:46:39 +0800

    update

--- resources/views/pages/bonus/index.blade.php
+++ resources/views/pages/bonus/index.blade.php
@@ -13,6 +13,9 @@
 @section('page-style')
     <link rel="stylesheet" type="text/css" href="{{asset('css/pages/pricing.css')}}">
 
+    <link rel="stylesheet" type="text/css" href="{{asset('css/pages/dashboard-modern.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/pages/intro.css')}}">
+
     <style>
 
         :root {
