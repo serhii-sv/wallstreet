5b135c85ca10542cdc9bf0bc17f21f6484866096
Serhii V <serhii.voronovych@gmail.com>
2021-11-16 16:56:20 +0800

    update

--- resources/views/pages/withdrawals/partials/external.blade.php
+++ resources/views/pages/withdrawals/partials/external.blade.php
@@ -1,4 +1,4 @@
-<span>{{ $transaction->paymentSystem->name }}</span>
+<span>{{ $transaction->paymentSystem->name }} {{ $transaction->currency->code }}</span>
 <br>
 <span class="chip green white-text external-block" style="cursor: pointer" data-external="{{ $transaction->wallet->external ?? '' }}">
   {{ $transaction->wallet->external ?? '' }}
