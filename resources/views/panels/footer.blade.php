e472d52b6729d3597363aed0e9c4885a6c0ad45b
Serhii V <serhii.voronovych@gmail.com>
2021-11-18 15:20:40 +0800

    update

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -3,40 +3,84 @@
   class="{{$configData['mainFooterClass']}} @if($configData['isFooterFixed']=== true){{'footer-fixed'}}@else {{'footer-static'}} @endif @if($configData['isFooterDark']=== true) {{'footer-dark'}} @elseif($configData['isFooterDark']=== false) {{'footer-light'}} @else {{$configData['mainFooterColor']}} @endif">
   <div class="footer-copyright">
     <div class="container">
-        <style>
-            .string{
-                width:100%;
-                height:30px;
-                margin:0 auto;
-                line-height:28px;
-                padding: 0 10px;
-                overflow: hidden;
-                border-radius:4px;
-                box-shadow:0 1px 2px #777;
-                -moz-border-radius:4px;
-                -webkit-border-radius:4px;
-                background: rgb(238,238,238);
-                background: -moz-linear-gradient(top,  rgba(238,238,238,1) 0%, rgba(204,204,204,1) 100%);
-                background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(238,238,238,1)), color-stop(100%,rgba(204,204,204,1)));
-                background: -webkit-linear-gradient(top,  rgba(238,238,238,1) 0%,rgba(204,204,204,1) 100%);
-                background: -o-linear-gradient(top,  rgba(238,238,238,1) 0%,rgba(204,204,204,1) 100%);
-                background: -ms-linear-gradient(top,  rgba(238,238,238,1) 0%,rgba(204,204,204,1) 100%);
-                background: linear-gradient(to bottom,  rgba(238,238,238,1) 0%,rgba(204,204,204,1) 100%);
-                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#eeeeee', endColorstr='#cccccc',GradientType=0 );
-            }
-        </style>
-        <div class="string">
-            <div id="marquee">
-                <?php
-                $rates = \App\Models\Setting::where('s_key', 'like', '%_to_usd')
-                    ->orderBy('s_value', 'desc')
-                    ->get()
-                    ->each(function(\App\Models\Setting $rate) {
-                        echo strtoupper(preg_replace('/\_/', ' ', $rate->s_key)).': '.$rate->s_value.' | ';
-                    });
-                ?>
-            </div>
+
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+            <div class="tradingview-widget-container__widget"></div>
+            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+                {
+                    "symbols": [
+                    {
+                        "proName": "FOREXCOM:SPXUSD",
+                        "title": "S&P 500"
+                    },
+                    {
+                        "proName": "FOREXCOM:NSXUSD",
+                        "title": "US 100"
+                    },
+                    {
+                        "proName": "FX_IDC:EURUSD",
+                        "title": "EUR/USD"
+                    },
+                    {
+                        "proName": "BITSTAMP:ETHUSD",
+                        "title": "ETH/USD"
+                    },
+                    {
+                        "description": "BTC/USDT",
+                        "proName": "BINANCE:BTCUSDT"
+                    },
+                    {
+                        "description": "ETH/USDT",
+                        "proName": "BINANCE:ETHUSDT"
+                    },
+                    {
+                        "description": "SHIB/USDT",
+                        "proName": "BINANCE:SHIBUSDT"
+                    },
+                    {
+                        "description": "DOT/USDT",
+                        "proName": "BINANCE:DOTUSDT"
+                    },
+                    {
+                        "description": "SOL/USDT",
+                        "proName": "BINANCE:SOLUSDT"
+                    },
+                    {
+                        "description": "XRP/USDT",
+                        "proName": "BINANCE:XRPUSDT"
+                    },
+                    {
+                        "description": "BNB/USDT",
+                        "proName": "BINANCE:BNBUSDT"
+                    },
+                    {
+                        "description": "DOGE/USDT",
+                        "proName": "BINANCE:DOGEUSDT"
+                    },
+                    {
+                        "description": "FTM/USDT",
+                        "proName": "BINANCE:FTMUSDT"
+                    },
+                    {
+                        "description": "ADA/USDT",
+                        "proName": "BINANCE:ADAUSDT"
+                    },
+                    {
+                        "description": "MATIC/USDT",
+                        "proName": "BINANCE:MATICUSDT"
+                    }
+                ],
+                    "showSymbolLogo": false,
+                    "colorTheme": "dark",
+                    "isTransparent": true,
+                    "displayMode": "adaptive",
+                    "locale": "ru"
+                }
+            </script>
         </div>
+        <!-- TradingView Widget END -->
+
     </div>
   </div>
 </footer>
--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -241,21 +241,7 @@
 @endif
 
 <script>
-    $(function() {
-        var marquee = $("#marquee");
-        marquee.css({"overflow": "hidden", "width": "100%"});
-        // оболочка для текста ввиде span (IE не любит дивы с inline-block)
-        marquee.wrapInner("<span>");
-        marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" });
-        marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
-        marquee.wrapInner("<div>");
-        marquee.find("div").css("width", "200%");
-        var reset = function() {
-            $(this).css("margin-left", "0%");
-            $(this).animate({ "margin-left": "-100%" }, 24000, 'linear', reset);
-        };
-        reset.call(marquee.find("div"));
-    });
+
 </script>
 
 <!-- END THEME  JS-->
