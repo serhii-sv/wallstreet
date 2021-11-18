35393dae12827043107f4bbc238085f65b96e80d
Serhii V <serhii.voronovych@gmail.com>
2021-11-18 13:03:44 +0800

    update

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -32,7 +32,8 @@
                     ->orderBy('s_value', 'desc')
                     ->get()
                     ->each(function(\App\Models\Setting $rate) {
-                        echo $rate->s_key.': '.$rate->s_value.' | ';
+                        preg_match('/([A-Za-z]+)\_to\_/([A-Za-z]+)', $rate->s_key, $match);
+                        echo strtoupper($match[1]).' to '.$match[2].': '.$rate->s_value.' | ';
                     });
                 ?>
             </div>
