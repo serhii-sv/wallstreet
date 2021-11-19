363897ef6779cabe21a01c9358c071966f8f638e
Serhii V <serhii.voronovych@gmail.com>
2021-11-19 12:08:53 +0800

    update

--- resources/views/pages/withdrawals/partials/external.blade.php
+++ resources/views/pages/withdrawals/partials/external.blade.php
@@ -1,5 +1,9 @@
 <span>{{ $transaction->paymentSystem->name }} {{ $transaction->currency->code }}</span>
 <br>
 <span class="chip green white-text external-block" style="cursor: pointer">
+  @if($transaction->paymentSystem->code == 'payeer')
+        {{ $transaction->wallet->external_payeer ?? '' }}
+  @else
   {{ $transaction->wallet->external ?? '' }}
+  @endif
 </span>
