fd9b795a81d6466206eba160d345a0a8631b7b1f
Serhii V <serhii.voronovych@gmail.com>
2021-11-25 12:56:07 +0800

    update

--- resources/views/pages/withdrawals/partials/upliner.blade.php
+++ resources/views/pages/withdrawals/partials/upliner.blade.php
@@ -1,8 +1,14 @@
 @php($partner = $transaction->user->firstPartner($transaction->user))
 @if(null !== $partner)
-    <span class="chip orange lighten-5">
+    <?php
+        $role = $partner->roles()->first();
+        $color = null !== $role && !empty($role->color)
+            ? $role->color
+            : null;
+    ?>
+    <span class="chip orange lighten-5"{{ null !== $color ? 'style="background:'.$color.';"' : '' }}>
             <a href="{{ route('users.show', $partner->id) }}">
-                <span class="orange-text">{{ $partner->login }}</span>
+                <span class="{{ null !== $color ? 'style="color:black; font-weight:bold;"' : 'orange-text' }}">{{ $partner->login }}</span>
             </a>
     </span>
 @endif
