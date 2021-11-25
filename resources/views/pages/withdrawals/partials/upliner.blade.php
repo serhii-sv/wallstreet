4c2003cec0b56d6593ad1e877b9d1e91c7baca4f
Serhii V <serhii.voronovych@gmail.com>
2021-11-25 13:00:35 +0800

    update

--- resources/views/pages/withdrawals/partials/upliner.blade.php
+++ resources/views/pages/withdrawals/partials/upliner.blade.php
@@ -6,7 +6,7 @@
         ? $role->color
         : null;
     ?>
-    <span class="chip {{ null !== $color ? '' : 'orange' }} lighten-5" title="{{ null !== $role ? $role->name : 'без роли' }}"{{ null !== $color ? 'style="background:'.$color.';"' : '' }}>
+    <span class="chip {{ null !== $color ? '' : 'orange' }} lighten-5" title="{{ null !== $role ? $role->name : 'без роли' }}" {{ null !== $color ? 'style=background:'.$color.';' : '' }}>
             <a href="{{ route('users.show', $partner->id) }}">
                 <span class="{{ null !== $color ? 'style="color:black; font-weight:bold;"' : 'orange-text' }}">{{ $partner->login }}</span>
             </a>
