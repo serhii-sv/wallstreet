11ffb1efd06bf78a684656c0ecd04fc2d1d31066
serhii <serhii.voronovych@gmail.com>
2021-12-02 14:32:31 +0800

    upd

--- resources/views/pages/replenishments/partials/repl_type.blade.php
+++ resources/views/pages/replenishments/partials/repl_type.blade.php
@@ -1,3 +1,9 @@
 <span class="invoice-amount">
+  @if($transaction->approved)
     {{ $transaction->is_real ? 'реал' : 'фейк' }}
+    @elseif($transaction->approved == 0)
+    ожидание
+    @elseif($transaction->approved == 2)
+    отменен
+    @endif
 </span>
