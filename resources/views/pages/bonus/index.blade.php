8f4afa9fa50ae5ac403d87f964be82dcda0db254
serhii <serhii.voronovych@gmail.com>
2021-12-02 16:47:46 +0800

    upd

--- resources/views/pages/bonus/index.blade.php
+++ resources/views/pages/bonus/index.blade.php
@@ -252,10 +252,10 @@
                                                placeholder="{{ __('Login, email or id') }}" value="{{ old('login') }}"
                                                style="font-weight: bold; text-align: center;width: 320px;">
 
-                                               <div class="input-field"  style="width:50%; margin-left:25%;">>
-                                                 <select class="select2 browser-default" name="login"
+                                               <div class="input-field"  style="width:50%; margin-left:25%;">
+                                                 <select class="select2 browser-default" name="login">
                                                    @foreach(\App\Models\User::select('login')->get() as $user)
-                                                   <option value="{{ $user->login }}">{{ $user->login }}</option>
+                                                   <option value="{{ $user->login }}">{{ $user->email }} ({{ $user->login }})</option>
                                                    @endforeach
                                                  </select>
                                                </div>
