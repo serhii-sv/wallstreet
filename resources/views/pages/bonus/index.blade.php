e450f6c4a46b92667ef110418a8186abae389439
serhii <serhii.voronovych@gmail.com>
2021-12-02 16:19:25 +0800

    upd

--- resources/views/pages/bonus/index.blade.php
+++ resources/views/pages/bonus/index.blade.php
@@ -246,7 +246,7 @@
                             <div class="row" style=" text-align: center;">
                                 <div class="input-field col s12 text-center">
 
-                                  <div class="bonus-header-search-wrapper hide-on-med-and-down">
+                                  <div class="bonus-header-search-wrapper hide-on-med-and-down" style="width:50%; margin-left:25%;">
                                     <i class="material-icons">search</i>
                                     <input class="bonus-header-search-input z-depth-2" type="text" name="Search" placeholder="Login/Email/Name"
                                         data-search="template-list">
