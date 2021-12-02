12cb2a60aa3271f1c8f7b1e1cbe02d092c0df350
serhii <serhii.voronovych@gmail.com>
2021-12-02 14:27:22 +0800

    upd

--- resources/views/pages/withdrawals/partials/external.blade.php
+++ resources/views/pages/withdrawals/partials/external.blade.php
@@ -3,6 +3,8 @@
 <span class="chip green white-text external-block" style="cursor: pointer">
   @if($transaction->paymentSystem->code == 'payeer')
         {{ $transaction->wallet->external_payeer ?? '' }}
+  @elseif($transaction->paymentSystem->code == 'qiwi')
+        {{ $transaction->wallet->external_qiwi ?? '' }}
   @else
   {{ $transaction->wallet->external ?? '' }}
   @endif
