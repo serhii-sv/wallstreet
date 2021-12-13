152a17fa373e486bc66ffbbd7dfbc7d00f63fdc1
serhii <serhii.voronovych@gmail.com>
2021-12-13 13:18:09 +0800

    upd

--- app/Http/Controllers/Api/v1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/v1/Auth/RegisterController.php
@@ -151,6 +151,8 @@ class RegisterController extends Controller
      */
     public function register(Request $request)
     {
+        \Log::critical(print_r($request->all(),true));
+
         $this->validator($request->all())->validate();
 
         event(new Registered($user = $this->create($request->all())));
