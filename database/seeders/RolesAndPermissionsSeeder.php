997abde000435a0b2ea0395c24e6d96ff32f8142
serhii <serhii.voronovych@gmail.com>
2021-12-14 12:26:00 +0800

    upd

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -18,21 +18,22 @@ class RolesAndPermissionsSeeder extends Seeder
     {
         app()['cache']->forget('spatie.permission.cache');
 
-        if (Role::where('name', 'root')->count() == 0) {
-            Role::create(['name' => 'root', 'color' => '#9c27b0']);
+        if (Role::where('name', 'boss')->count() == 0) {
+            Role::create(['name' => 'boss', 'color' => '#9c27b0']);
             echo "Role 'root' registered.\n";
         } else {
-            Role::where('name', 'root')->update(['is_fixed' => true]);
+            Role::where('name', 'boss')->update(['is_fixed' => true]);
             echo "Role 'root' already registered.\n";
         }
 
-        if (Role::where('name', 'admin')->count() == 0) {
-            Role::create(['name' => 'admin', 'color' => '#ff4081']);
-            echo "Role 'admin' registered.\n";
-        } else {
-            Role::where('name', 'admin')->update(['is_fixed' => true]);
-            echo "Role 'admin' already registered.\n";
-        }
+//        if (Role::where('name', 'admin')->count() == 0) {
+//            Role::create(['name' => 'admin', 'color' => '#ff4081']);
+//            echo "Role 'admin' registered.\n";
+//        } else {
+//            Role::where('name', 'admin')->update(['is_fixed' => true]);
+//            echo "Role 'admin' already registered.\n";
+//        }
+
         if (Role::where('name', 'teamlead')->count() == 0) {
             Role::create(['name' => 'teamlead', 'color' => '#3b6eff']);
             echo "Role 'teamlead' registered.\n";
