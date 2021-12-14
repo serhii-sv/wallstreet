9622a32054dce0a6406b896935b776757539cf40
serhii <serhii.voronovych@gmail.com>
2021-12-14 12:29:41 +0800

    upd

--- resources/views/pages/replenishments/partials/replenished.blade.php
+++ resources/views/pages/replenishments/partials/replenished.blade.php
@@ -1,11 +1,15 @@
 @if($transaction->batch_id)
     {{ null !== $transaction->paymentSystem ? $transaction->paymentSystem->name : 'без платежной системы' }}
+@elseif(!empty($transaction->source))
+    @php($partner = \App\Models\User::where('email', $transaction->source)->first())
+    @if(null !== $partner)
+        <a href="{{ route('users.show', $partner->id) }}">
+            {{ $partner->login }}
+        </a>
+    @endif
 @else
     @php($partner = $transaction->user->firstPartner($transaction->user))
     @if(null !== $partner)
-        <?php
-        $role = $partner->roles()->first();
-        ?>
         <a href="{{ route('users.show', $partner->id) }}">
             {{ $partner->login }}
         </a>
