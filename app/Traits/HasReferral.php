0c199769890255202222ce6b6ae710fd7249b68b
serhii <serhii.voronovych@gmail.com>
2021-12-26 10:44:19 +0800

    upd

--- app/Traits/HasReferral.php
+++ app/Traits/HasReferral.php
@@ -235,7 +235,8 @@ trait HasReferral
         // TODO: problems with cash here
 //        return cache()->remember('referrals_array.'.$th->id.$level.$max, now()->addMinutes(60), function() use($th, $level, $max) {
             /** @var User $referrals */
-            $referrals = $th->referrals()->wherePivot('line', 1)->get();
+//            $referrals = $th->referrals()->wherePivot('line', 1)->get();
+            $referrals = User::where('partner_id', $this->my_id)->get();
 
             $result = [];
 
