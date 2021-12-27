edd6cfa4083b01f4b7505d291ce8b209fc243ba8
serhii <serhii.voronovych@gmail.com>
2021-12-27 21:13:10 +0800

    upd

--- app/Http/Controllers/ReplenishmentController.php
+++ app/Http/Controllers/ReplenishmentController.php
@@ -43,8 +43,8 @@ class ReplenishmentController extends Controller
                 /** @var User $user */
                 $user = User::where('id', $request->user)->first();
 
-                $referrals = cache()->remember('user.referrals_10' . $user->id, 180, function () use ($user) {
-                    return $user->getAllReferralsInArray(1, 10);
+                $referrals = cache()->remember('user.referrals_1' . $user->id, 180, function () use ($user) {
+                    return $user->getAllReferralsInArray(1, 1);
                 });
 
                 $ids = [];
