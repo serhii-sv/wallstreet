0f9111b3b5f07c571296cbdc2000da4058fee5a6
serhii <serhii.voronovych@gmail.com>
2022-01-04 09:49:14 +0800

    upd

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -382,7 +382,6 @@ class UsersController extends Controller
 
             if ($request->roles[0] == $oldRole) {
                 if ($request->permissions) {
-                    die('sync '.$oldRole.'/'.$request->roles[0]);
                     $user->syncPermissions($request->permissions);
                 }
             }
