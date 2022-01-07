8986ec4c6125affbf44a52383342e147c0cb2251
serhii <serhii.voronovych@gmail.com>
2022-01-07 19:46:09 +0800

    upd

--- app/Console/Commands/TransactionTeamleaderCommand.php
+++ app/Console/Commands/TransactionTeamleaderCommand.php
@@ -54,7 +54,7 @@ class TransactionTeamleaderCommand extends Command
 
         $processed = 0;
 
-        foreach ($transactions->chunk(5000) as $chunk) {
+        foreach ($transactions->chunk(100) as $chunk) {
 
             $queries = '';
 
