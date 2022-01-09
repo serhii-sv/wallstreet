d32021c60c48395ec92a96d0d07d8c64d79fd816
serhii <serhii.voronovych@gmail.com>
2022-01-09 17:06:56 +0800

    upd

--- resources/views/pages/withdrawals/partials/user-item.blade.php
+++ resources/views/pages/withdrawals/partials/user-item.blade.php
@@ -1,4 +1,7 @@
 <a href="{{ route('users.show', $transaction->user->id) }}" style="color:{{ $transaction->user->getRoleColor() }};">{{ $transaction->user->email }}</a>
 <br>
 (<a href="{{ route('users.show', $transaction->user->id) }}" style="color:{{ $transaction->user->getRoleColor() }};">{{ $transaction->user->login }}</a>)
-
+@if(!empty($transaction->withdraw_reason))
+<br>
+<strong style="color:red;">Заявка была возвращена из очереди на обработку. Причина: {{ $transaction->withdraw_reason }}</strong>
+@endif
